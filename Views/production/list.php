a412c52345104899811a593929b392a74812b996
RyanM <ryan.mazri@yahoo.fr>
2023-02-08 00:56:25 +0100

    v3.3.2: fixed route production studio detail

--- Views/production/list.php
+++ Views/production/list.php
@@ -2,7 +2,7 @@
     <div>
         <h2>
             <div class="card">
-                <a href="/anime/detail/<?= $production[$i]->id ?>">
+                <a href="/production/detail/<?= $production[$i]->id ?>">
                     <img src="<?= $production[$i]->image ?>" alt="Image">
                     <?= $production[$i]->name ?>
 
